96f255596a83e67d4a2ebf5573e9d6a7ebbd13d1
kawtare12 <120519708+kawtare12@users.noreply.github.com>
2023-05-21 22:19:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,9 +22,13 @@ app.use('/api/pharmacies', pharmacieController);
 
 
 
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 
 });
+app.get('/',(req,res)=>{
+  res.send("hello")
+})
 
